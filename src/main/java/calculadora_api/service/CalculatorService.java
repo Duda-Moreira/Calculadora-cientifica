e59ce09c49067a72e128fc5d028089eb24dbd5e8
Duda <dudamoreira13047@gmail.com>
2026-08-17 11:49:13 -0400

    feat: implementando operações de aritmética básica

--- src/main/java/calculadora_api/service/CalculatorService.java
+++ src/main/java/calculadora_api/service/CalculatorService.java
@@ -18,6 +18,38 @@ public class CalculatorService {
             return a + b;
         }
 
+        if (expression.contains("-")) {
+            String[] parts = expression.split("-");
+
+            double a = Double.parseDouble(parts[0]);
+            double b = Double.parseDouble(parts[1]);
+
+            return a - b;
+        }
+
+        if (expression.contains("*")) {
+            String[] parts = expression.split("\\*");
+
+            double a = Double.parseDouble(parts[0]);
+            double b = Double.parseDouble(parts[1]);
+
+            return a * b;
+        }
+
+        if (expression.contains("/")) {
+            String[] parts = expression.split("/");
+
+            double a = Double.parseDouble(parts[0]);
+            double b = Double.parseDouble(parts[1]);
+
+            if (b == 0) {
+                throw new IllegalArgumentException("Divisão por zero não permitida.");
+            }
+
+            return a / b;
+        }
+
+
         throw new IllegalArgumentException("Operação não suportada.");
     }
 }
