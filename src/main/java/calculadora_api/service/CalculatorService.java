9f6bc569f1b9736b79fa222c3053dbc411266cac
Duda <dudamoreira13047@gmail.com>
2026-08-17 12:06:04 -0400

    feat: integrar o avaliador de expressões exp4j

--- src/main/java/calculadora_api/service/CalculatorService.java
+++ src/main/java/calculadora_api/service/CalculatorService.java
@@ -1,55 +1,16 @@
 package calculadora_api.service;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-    
     public double calculate(String expression) {
-
-        expression = expression.replace(" ", "");
-
-        if (expression.contains("+")) {
-            String[] parts = expression.split("\\+");
-
-            double a = Double.parseDouble(parts[0]);
-            double b = Double.parseDouble(parts[1]);
-
-            return a + b;
-        }
-
-        if (expression.contains("-")) {
-            String[] parts = expression.split("-");
-
-            double a = Double.parseDouble(parts[0]);
-            double b = Double.parseDouble(parts[1]);
-
-            return a - b;
-        }
-
-        if (expression.contains("*")) {
-            String[] parts = expression.split("\\*");
-
-            double a = Double.parseDouble(parts[0]);
-            double b = Double.parseDouble(parts[1]);
-
-            return a * b;
-        }
-
-        if (expression.contains("/")) {
-            String[] parts = expression.split("/");
-
-            double a = Double.parseDouble(parts[0]);
-            double b = Double.parseDouble(parts[1]);
-
-            if (b == 0) {
-                throw new IllegalArgumentException("Divisão por zero não permitida.");
-            }
-
-            return a / b;
-        }
-
-
-        throw new IllegalArgumentException("Operação não suportada.");
+        Expression exp = new ExpressionBuilder(expression)
+            .build();
+        
+            return exp.evaluate();
     }
 }
